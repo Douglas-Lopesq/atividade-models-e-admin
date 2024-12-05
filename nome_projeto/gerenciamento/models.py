a8a209032fc9b3e57717ba8343293965e75dfe15
Douglas-Lopesq <88059347+Douglas-Lopesq@users.noreply.github.com>
2024-12-05 16:10:37 -0300

    Add files via upload

---
+++ nome_projeto/gerenciamento/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255, verbose_name="Nome do Produto")
+    codigo = models.CharField(max_length=100, unique=True, verbose_name="Código do Produto")
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição")
+    preco = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
+    quantidade = models.IntegerField(verbose_name="Quantidade em Estoque")
+    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
+
+    class Meta:
+        ordering = ['-data_criacao']
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+
+    def __str__(self):
+        return f"{self.nome} ({self.codigo})"
